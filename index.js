ae3eee2b839ca624f5e64d4f882b13acce699dda
unknown <mahadarisandeep@gmail.com>
2021-07-31 18:05:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express  = require('express')
+const app = express()
+const { v4: uuidv4 } = require('uuid');
+
+app.use(express.json())
+ 
+let users = []
+
+app.get('/users', (req, res) => {
+    res.send(users);
+})
+
+app.post('/users', (req, res) => {
+    const user = req.body;
+    console.log(user)
+    users.push({...user, id:uuidv4()})
+    res.send('user created')
+})
+app.get('/user/:id', (req, res) => {
+    const id = req.params.id
+    const founduser = users.find((user) => user.id === id)
+    res.send(founduser)
+
+})
+
+app.delete('/user/:id', (req, res) => {
+    const id = req.params.id
+    users = users.filter(user => user.id !== id)
+    res.status(200).send({message:'user deleted'}) 
+})
+
+
+
+
+
+
+
+
+const port = 4000;
+app.listen(port, () => {
+    console.log('server is running on port 4000')
+})
